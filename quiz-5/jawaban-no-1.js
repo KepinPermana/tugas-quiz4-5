14922b8cb10b7c9e4a449a6363005b34638b2dd4
unknown <kepinpermana98@gmail.com>
2022-06-05 00:00:07 +0700

    4&5-new

--- quiz-5/jawaban-no-1.js
+++ quiz-5/jawaban-no-1.js
@@ -3,3 +3,9 @@ nilai.forEach(function (nilai) {
   console.log(nilai);
   // panggil fungsi bersama parameter
 });
+// ada metode lain yang bisa dipecahkan
+var nilai =12;
+
+for (let index = 0; index < 6; index++) {
+  console.log(index*nilai);
+}
